1fe2de02503a3c0b66e999ad10197e0327142aa3
goblincore <withallmyhrt@gmail.com>
2023-06-29 22:05:07 -0400

    Add Media collection, custom route to custom component test

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,18 +2,34 @@ import { buildConfig } from 'payload/config';
 import path from 'path';
  import Examples from './collections/Examples';
  import Credentials from './collections/Credentials';
+ import Media from './collections/Media';
  import Students from './collections/Students';
  import CredentialsTemplatesCollection from './collections/CredentialTemplates';
 import Users from './collections/Users';
+
+import CustomDefaultRoute from './components/CustomComponent';
 
 export default buildConfig({
   serverURL: 'http://localhost:3000',
   admin: {
     user: Users.slug,
+    components: {
+      routes: [
+        {
+          path: '/issue-credentials',
+          Component: CustomDefaultRoute,
+        },
+        {
+          path: '/test',
+          Component: CustomDefaultRoute,
+        },
+      ],
+    },
   },
   collections: [
     Users,
     // Add Collections here
+    Media,
     Students,
     Credentials,
     CredentialsTemplatesCollection,
